442f53421e99d25c0d61a21af1863bf2794ea717
Xelbor <xelbor45hail@gmail.com>
2025-12-25 22:47:49 +0500

    large update, added balance and referal system

--- app/handlers.py
+++ app/handlers.py
@@ -1,54 +1,78 @@
 from aiogram import types, F, Router, Bot
 from aiogram.filters import CommandStart, Command
 from datetime import datetime, timedelta
+import datetime
 from app.large_texts import *
 import app.utils as utils
 import asyncio
 
 router = Router()
 
-# -------------------- FREE GIFT --------------------
-async def give_gift(message: types.Message):
-    subscribe_link = utils.create_a_subscribe_link(-864000000)
-
-    with utils.sqlite3.connect("users.db") as conn:
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO trials (user_id, vpn_subscribe, start_date) VALUES (?, ?, date('now'))",
-                       (message.from_user.id, subscribe_link))
-        conn.commit()
-
-    await message.answer(gift_text, parse_mode="HTML")
-    await message.answer(f"<tg-spoiler>{subscribe_link}</tg-spoiler>", parse_mode='HTML')
-
-
 # -------------------- START --------------------
 @router.message(CommandStart())
 async def main(message: types.Message):
-    user_id = message.from_user.id
+    args = message.text.split()
+    ref_code = args[1] if len(args) > 1 else None
+
     with utils.sqlite3.connect("users.db") as conn:
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT OR IGNORE INTO chats (user_id) VALUES (?)",
-            (user_id,)
+            "INSERT OR IGNORE INTO users (user_id, created_at) VALUES (?, ?)",
+            (message.from_user.id, datetime.datetime.now(datetime.UTC))
         )
-        conn.commit()
+        
+        if ref_code and ref_code.startswith("ref_"):
+            code = ref_code.replace("ref_", "")
+
+            cursor.execute(
+                "SELECT referrer_id FROM users WHERE user_id = ?",
+                (message.from_user.id,)
+            )
+            if cursor.fetchone()[0] is None:
+                cursor.execute(
+                    "SELECT user_id FROM users WHERE referrer_id = ?",
+                    (code,)
+                )
+                ref_owner = cursor.fetchone()
+
+                if ref_owner and ref_owner[0] != message.from_user.id:
+                    cursor.execute(
+                        "UPDATE users SET referrer_id = ? WHERE user_id = ?",
+                        (ref_owner[0], message.from_user.id)
+                    )
+
+                    cursor.execute(
+                        """INSERT INTO refs (referrer_id, invited_id, bonus, created_at)
+                           VALUES (?, ?, ?, ?)""",
+                        (ref_owner[0], message.from_user.id, 0, datetime.datetime.now(datetime.UTC))
+                    )
+
+                    conn.commit()
+                    utils.add_balance(ref_owner[0], 50)
 
     markup = types.ReplyKeyboardMarkup(
         keyboard=[
-            [types.KeyboardButton(text="🔑 Мои ключи"), types.KeyboardButton(text="📲 Установка")],
-            [types.KeyboardButton(text="💳 Купить подписку"), types.KeyboardButton(text="🎁 Бесплатный период")],
-            [types.KeyboardButton(text="❓ Помощь")]
+            [types.KeyboardButton(text="🔑 Мои ключи"), types.KeyboardButton(text="💳 Пополнить баланс")],
+            [types.KeyboardButton(text="🎁 Бесплатный период"), types.KeyboardButton(text="💰 Баланс")],
+            [types.KeyboardButton(text="📲 Установка"), types.KeyboardButton(text="💸 Скидка")]
         ],
         resize_keyboard=True
     )
 
+    refs_inline = types.InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                types.InlineKeyboardButton(text='Как получить скидку 70%?', callback_data="refs_call"),
+            ],
+        ]
+    )
+
     await message.answer(welcome_text, reply_markup=markup, parse_mode='HTML')
+    await message.answer("ℹ️ Узнай, как получить скидку:", reply_markup=refs_inline)
 
-# -------------------- HELP --------------------
-@router.message(Command('help'))
-async def help_cmd(message: types.Message):
-    await message.answer(help_text, parse_mode="HTML")
-
+@router.callback_query(F.data.startswith("refs_"))
+async def refs_callback(call: types.CallbackQuery):
+    await referal_system(call.message)
 
 # -------------------- BUY MENU --------------------
 @router.message(Command('buy'))
@@ -62,6 +86,9 @@ async def buy(message: types.Message):
             [
                 types.InlineKeyboardButton(text='💳 90 дней — 249₽', callback_data="buy_90"),
                 types.InlineKeyboardButton(text='💳 180 дней — 449₽', callback_data="buy_180")
+            ],
+            [
+                types.InlineKeyboardButton(text='💳 Своя сумма', callback_data="buy_own"),
             ]
         ]
     )
@@ -70,33 +97,35 @@ async def buy(message: types.Message):
 
 @router.callback_query(F.data.startswith("buy_"))
 async def callback_buy(call: types.CallbackQuery):
-    prices = {
-        "buy_7": ("Подписка на 7 дней", 7500),
-        "buy_30": ("Подписка на 30 дней", 9900),
-        "buy_90": ("Подписка на 90 дней", 24900),
-        "buy_180": ("Подписка на 180 дней", 44900)
-    }
+    if (call.data == "buy_own"):
+        await call.message.answer("💰 Введите сумму на которую хотите пополнить баланс:")
+    else:
+        prices = {
+            "buy_7": ("Баланс на 7 дней подписки", 7500),
+            "buy_30": ("Баланс на 30 дней подписки", 9900),
+            "buy_90": ("Баланс на 90 дней подписки", 24900),
+            "buy_180": ("Баланс на 180 дней подписки", 44900)
+        }
 
-    title, amount = prices[call.data]
+        title, amount = prices[call.data]
 
-    markup = types.InlineKeyboardMarkup(
-        inline_keyboard=[
-            [types.InlineKeyboardButton(text="Оплатить 💳", callback_data=f"pay_{call.data}")],
-            [types.InlineKeyboardButton(text="◀️ Назад", callback_data="back_to_buy")]
-        ]
-    )
+        markup = types.InlineKeyboardMarkup(
+            inline_keyboard=[
+                [types.InlineKeyboardButton(text="Оплатить 💳", callback_data=f"pay_{call.data}")],
+                [types.InlineKeyboardButton(text="◀️ Назад", callback_data="back_to_buy")]
+            ]
+        )
 
-    await call.message.edit_text(
-        f"Вы выбрали: <b>{title}</b>\nНажмите 'Оплатить' чтобы продолжить.",
-        parse_mode="HTML",
-        reply_markup=markup
-    )
+        await call.message.edit_text(
+            f"Вы выбрали: <b>{title}</b>\nНажмите 'Оплатить' чтобы продолжить.",
+            parse_mode="HTML",
+            reply_markup=markup
+        )
 
 
 @router.callback_query(F.data == "back_to_buy")
 async def buy_back(call: types.CallbackQuery):
     await buy(call.message)
-
 
 # -------------------- PAYMENT --------------------
 @router.callback_query(F.data.startswith("pay_"))
@@ -105,15 +134,15 @@ async def callback_pay(call: types.CallbackQuery):
     tariff = call.data.replace("pay_", "")
 
     prices = {
-        "buy_7": ("Подписка на 7 дней", 7500),
-        "buy_30": ("Подписка на 30 дней", 9900),
-        "buy_90": ("Подписка на 90 дней", 24900),
-        "buy_180": ("Подписка на 180 дней", 44900)
+        "buy_7": ("Баланс на 7 дней подписки", 7500),
+        "buy_30": ("Баланс на 30 дней подписки", 9900),
+        "buy_90": ("Баланс на 90 дней подписки", 24900),
+        "buy_180": ("Баланс на 180 дней подписки", 44900)
     }
 
     title, amount = prices[tariff]
 
-    await send_invoice(bot, call.message.chat.id, amount, title, tariff)
+    await send_invoice(bot, call.message.from_user.id, amount, title, tariff)
 
 async def send_invoice(bot, chat_id, amount, title, tariff):
     prices = [types.LabeledPrice(label=title, amount=amount)]
@@ -121,7 +150,7 @@ async def send_invoice(bot, chat_id, amount, title, tariff):
     await bot.send_invoice(
         chat_id=chat_id,
         title=title,
-        description=f"Оформление подписки: {title}\n" + "Оплата проводится через официальную площадку Юкасса",
+        description=f"Оформление пополнения баланса: {title}\n" + "Оплата проводится через официальную площадку Юкасса",
         payload=tariff,
         provider_token=utils.PAYMENT_TOKEN,
         currency="RUB",
@@ -133,13 +162,14 @@ async def send_invoice(bot, chat_id, amount, title, tariff):
 async def handle_successful_payment(pre_checkout_q: types.PreCheckoutQuery):
     await pre_checkout_q.answer(ok=True)
 
+# --------------- SUCCESSFUL PAYMENT --------------------
 @router.message(F.successful_payment)
 async def successful_payment(message: types.Message):
     payment_info = message.successful_payment
 
     total_amount = payment_info.total_amount
     title = payment_info.invoice_payload
-
+        
     match title:
         case "buy_7":
             time_remain = int(timedelta(days=7).total_seconds())
@@ -149,16 +179,13 @@ async def successful_payment(message: types.Message):
             time_remain = int(timedelta(days=90).total_seconds())
         case "buy_180":
             time_remain = int(timedelta(days=180).total_seconds())
-
-    new_subscribe_link = utils.create_a_subscribe_link(-time_remain * 1000)
+        
+    utils.add_balance(message.from_user.id, total_amount/100)
+    user_balance = utils.get_balance(message.from_user.id)
 
     await message.answer(
-        "🎉 Оплата прошла успешно!\n"
-        f"Сумма: {total_amount/100} ₽\n\n"
-        "Ваш ключ:\n"
-        f"<tg-spoiler>{new_subscribe_link}</tg-spoiler>",
-        parse_mode="HTML"
-    )    
+        "🎉 Баланс пополнен успешно!\n"
+        f"💰 Ваш текущий баланс: {user_balance}")
 
 # -------------------- MANUAL --------------------
 @router.message(Command('manual'))
@@ -213,11 +240,22 @@ async def check_key(message: types.Message):
     else:
         await message.answer(
             "У вас пока нет ключа.\n\n"
-            "Нажмите кнопку 🎁 Бесплатный период, или купите, нажав кнопку 💳 Купить подписку"
+            "Нажмите кнопку 🎁 Бесплатный период, или пополните баланс, нажав кнопку 💳 Пополнить баланс"
         )
 
+# -------------------- FREE GIFT --------------------
+async def give_gift(message: types.Message):
+    subscribe_link = utils.create_a_subscribe_link(-864000000)
 
-# -------------------- GIFT --------------------
+    with utils.sqlite3.connect("users.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO trials (user_id, vpn_subscribe, start_date) VALUES (?, ?, date('now'))",
+                       (message.from_user.id, subscribe_link))
+
+    await message.answer(gift_text, parse_mode="HTML")
+    await message.answer(f"<tg-spoiler>{subscribe_link}</tg-spoiler>", parse_mode='HTML')
+
+# ---------------------- GIFT --------------------
 @router.message(Command('gift'))
 async def gift(message: types.Message):
     if utils.check_users_gift(message.from_user.id):
@@ -225,29 +263,26 @@ async def gift(message: types.Message):
     else:
         await give_gift(message)
 
-@router.message(Command('send_tech_works'))
-async def tech_works(message: types.Message):
-    print(utils.SERVICE_CHAT_ID)
-    if (message.chat.id == utils.SERVICE_CHAT_ID):
-        await message.answer(str(message.chat.id))
-
-# ----------------------- BROADCAST ----------------------
+# ---------------------- Broadcast ----------------------
 @router.message(Command('broadcast'))
 async def broadcast_handler(message: types.Message):
-    if(message.from_user.id != utils.SERVICE_CHAT_ID):
+    if(message.from_user.id == int(utils.SERVICE_ID)):
+        text = message.text.replace("/broadcast", "", 1).strip()
+        if not text:
+            await message.answer("Текст рассылки пуст")
+            return
+        if (text == "tech_works"):
+            await broadcast(message.bot, "test tech works")
+        else:
+            await broadcast(message.bot, text)
+    else:
         await message.answer("Нет прав на выполнение команды")
         return
-    
-    text = message.text.replace("/broadcast", "", 1).strip()
-    if not text:
-        await message.answer("Текст рассылки пуст")
-        return
-    await broadcast(message.bot, text)
 
 async def broadcast(bot: Bot, text: str):
     with utils.sqlite3.connect("users.db") as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT user_id FROM chats")
+        cursor.execute("SELECT user_id FROM users")
         users = cursor.fetchall()
 
         for (user_id,) in users:
@@ -255,18 +290,43 @@ async def broadcast(bot: Bot, text: str):
                 await bot.send_message(user_id, text)
                 await asyncio.sleep(0.05)  # защита от FloodWait
             except Exception:
-                pass  # пользователь мог заблокировать бота
+                pass
+
+# ----------------------- Balance ------------------------
+@router.message(Command('balance'))
+async def balance(message: types.Message):
+    user_balance = utils.get_balance(message.from_user.id)
+    await message.answer(f"💰 <b>Ваш текущий баланс:</b> {str(user_balance)}₽", parse_mode='HTML')
+    
+# -------------------- Referal System --------------------
+@router.message(Command('referal'))
+async def referal_system(message: types.Message):
+    if utils.user_has_invites(message.from_user.id):
+        await message.answer("<b>Вы уже использовали свою реферальную ссылку.</b> 👀 По ней уже был зарегистрирован человек", parse_mode='HTML')
+    else:
+        await message.answer(ref_text, parse_mode='HTML')
+        random_id = utils.random_email(6)
+        with utils.sqlite3.connect("users.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT referrer_id FROM users WHERE user_id = ?", (message.chat.id,))
+
+            if cursor.fetchone()[0] is None:
+                cursor.execute("UPDATE users SET referrer_id = ? WHERE user_id = ?", (random_id, message.chat.id))
+                link = f"{utils.BOT_LINK}?start=ref_{random_id}"
+                await message.answer("🎁 <b>Вот ваша ссылка для друга:</b> \n" + link, parse_mode='HTML')
 
 # -------------------- MARKUP BUTTONS --------------------
 @router.message()
 async def handle_markup_keyboard(message: types.Message):
     if message.text == "🔑 Мои ключи":
         await check_key(message)
-    elif message.text == "📲 Установка":
-        await manual(message)
-    elif message.text == "💳 Купить подписку":
+    elif message.text == "💳 Пополнить баланс":
         await buy(message)
     elif message.text == "🎁 Бесплатный период":
         await gift(message)
-    elif message.text == "❓ Помощь":
-        await help_cmd(message)
+    elif message.text == "💰 Баланс":
+        await balance(message)
+    elif message.text == "📲 Установка":
+        await manual(message)
+    elif message.text == "💸 Скидка":
+        await referal_system(message)
--- app/utils.py
+++ app/utils.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_LINK = os.getenv("BOT_LINK")
 PAYMENT_TOKEN = os.getenv("PAYMENT_TOKEN")
 
 # Используем один сервер
@@ -20,23 +21,68 @@ USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 PASSPHRASE = os.getenv("PASSPHRASE")
 
-SERVICE_CHAT_ID = os.getenv("SERVICE_CHAT_ID")
+SERVICE_ID = os.getenv("SERVICE_ID")
 
 db = sqlite3.connect('users.db')
 cursor = db.cursor()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS trials (
-               user_id INTEGER PRIMARY KEY,
+               user_id BIGINT PRIMARY KEY,
                vpn_subscribe TEXT,
                start_date TEXT
-)""")
+)""")          # Решить со start_date
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS chats (
-               user_id INTEGER PRIMARY KEY
-)""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+               user_id BIGINT PRIMARY KEY,
+               balance INTEGER DEFAULT 0,
+               referrer_id BIGINT NULL,
+               created_at TIMESTAMP
+)""")          # Таблица для всех пользователей
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS refs (
+               referrer_id BIGINT,
+               invited_id BIGINT,
+               bonus INTEGER,
+               created_at TIMESTAMP
+)""")          # Таблица для пользователей УЖЕ учавствующих в реферальной программе
 
 db.commit()
 db.close()
+
+def user_has_referrer(user_id: int) -> bool:
+    with sqlite3.connect("users.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT 1 FROM users WHERE user_id = ? AND referrer_id IS NOT NULL", (user_id,)) # Нужно как-то реализовать проверку вот здесь
+        return cursor.fetchone() is not None
+    
+def user_has_invites(user_id: int) -> bool:
+    with sqlite3.connect("users.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT 1 FROM refs WHERE referrer_id = ? LIMIT 1",
+            (user_id,)
+        )
+        return cursor.fetchone() is not None
+    
+def get_balance(user_id):
+    with sqlite3.connect("users.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT balance FROM users WHERE user_id = ?", (user_id,))
+        result = cursor.fetchone()
+        if result is not None:
+            balance = result[0]
+        else:
+            balance = 0
+        return balance
+
+    
+def add_balance(user_id, balance):
+    with sqlite3.connect("users.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "UPDATE users SET balance = balance + ? WHERE user_id = ?",
+            (balance, user_id)
+        )
 
 def check_users_gift(user_id):
     with sqlite3.connect("users.db") as conn:
